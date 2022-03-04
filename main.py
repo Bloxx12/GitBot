fb02f43080a8e268eed18c035081e8bf3af46682
Bloxx12 <75451918+Bloxx12@users.noreply.github.com>
2022-03-04 10:03:54 +0100

    A bunch of Changes

--- main.py
+++ main.py
@@ -2,18 +2,21 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello() -> str:
-    return 'Hello World!'
+#@app.route('/')
+#def hello() -> str:
+#    return 'Hello World!'
 
-@app.route('/index.html', methods=['GET'])
-def inputting() -> 'html':
+@app.route('/temp', methods=['GET'])
+def hello3() -> 'html':
+    return render_template('template2.html')
+
+@app.route('/', methods=['GET'])
+def inputfunc() -> 'html':
     return render_template('template1.html')
 
 @app.route('/test', methods=['POST'])
-def hello2() -> 'html':
-        eingabeeins = request.form['eingabeeins']
-    return render_template('results.html', 
-                            inputeins = eingabeeins,
-                            )
+def results() -> 'html':
+    return render_template('results.html',
+                            inputeins = request.form['eingabeeins'],
+                            inputzwei = request.form['eingabezwei'])
 app.run()
